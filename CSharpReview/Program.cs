eb44a3512574ebc12138618413e16654581050f2
Chris Caudill <christopherscaudill@gmail.com>
2019-02-11 16:23:45 -0500

    init-c

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -13,3 +13,30 @@ namespace CSharpReview
         }
     }
 }
+//3 tenets of oop?
+//polymorphism, inheritance, encapsulation (PIE)
+
+//ternary operator?
+//coditional operator (?:), evaluates a boolean expression
+// var x = (condition) ? (true "consequence") : (false "alternative")
+
+//what the foreach statement does?
+//executes a statement or a block of statements "for each" element in an instance of the type.
+//"iteration through a collection"
+// foreach (var item in collection)
+
+//know how to index into a array?
+
+//what can be the contents of an interface?
+
+//when do you have a generic collection class, what type of data can be entered into it?
+
+//what method must exist in any c# executable program?
+//the main method
+
+//how to determine the length of a string?
+
+//what data is needed for a dictionary?
+//key/values
+
+//difference between a nullable int and a int?
